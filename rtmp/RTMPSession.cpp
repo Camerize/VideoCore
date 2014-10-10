b56eeb559ca076e5ab4698eba6f90a24fa283abf
Stefanos Kornilios Mitsis Poiitids <skmp@nillware.com>
2014-10-10 21:16:41 +0300

    Fix streaming timeout pointer reset

--- rtmp/RTMPSession.cpp
+++ rtmp/RTMPSession.cpp
@@ -312,7 +312,7 @@ namespace videocore
                 }
             }
             
-        } while((m_streamSession->status() & kStreamStatusReadBufferHasBytes) && !stop2);
+        } while(m_streamSession && (m_streamSession->status() & kStreamStatusReadBufferHasBytes) && !stop2);
     }
     void
     RTMPSession::setClientState(ClientState_t state)
